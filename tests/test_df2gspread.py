3694efa98cde95b66f020ff6de84ebc7680d9621
etrott <etrott@redhat.com>
2015-09-15 09:30:53 +0200

    Connect travis-ci 3

--- tests/test_df2gspread.py
+++ tests/test_df2gspread.py
@@ -4,22 +4,11 @@
 # @Date:   2015-09-16 13:22:22
 # @Email:  etrott@redhat.com
 # @Last modified by:   etrott
-# @Last Modified time: 2015-09-16 13:23:23
+# @Last Modified time: 2015-09-16 13:23:56
 
 
 from __future__ import unicode_literals, absolute_import
 
-import pytest
-
-
-# MAKE THIS SO IT ONLY EVER GETS RUN ONCE PER "SESSION"
-def test_global_import():
-    from df2gspread import df2gspread
-
-
-from __future__ import unicode_literals, absolute_import
-
-from instructions import commands, datatypes
 import pytest
 
 
