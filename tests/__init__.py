43849defebf145483dc54b12a5509e4b13dd9b9e
etrott <etrott@redhat.com>
2015-09-15 09:25:24 +0200

    Add tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,4 +4,8 @@
 # @Date:   2015-09-16 13:21:01
 # @Email:  etrott@redhat.com
 # @Last modified by:   etrott
+<<<<<<< HEAD
 # @Last Modified time: 2015-09-16 13:21:21
+=======
+# @Last Modified time: 2015-09-16 13:12:18
+>>>>>>> Add tests
