59ad57f0ad68d913d276fce5d497a8019d1b9f71
etrott <etrott@redhat.com>
2015-09-15 09:25:24 +0200

    Add tests

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Eduard Trott
+# @Date:   2015-09-08 16:31:30
+# @Email:  etrott@redhat.com
+# @Last modified by:   etrott
+# @Last Modified time: 2015-09-08 16:31:30
