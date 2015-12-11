c59cd3e1b581366444bec9a9fd3d7a5e4241aea8
Nick Roth <nroth@dealnews.com>
2015-12-11 10:45:21 -0600

    Add and refactor tests so that it marks tests to fail only when they should fail.

---
+++ tests/conftest.py
@@ -0,0 +1,41 @@
+
+import pytest
+
+from df2gspread.utils import (get_credentials,
+                              create_service_credentials)
+
+
+@pytest.fixture
+def service_credentials():
+
+    sc = dict(key_file='~/.df2gspread_secrets.p12',
+              client_file='~/.gdrive_service_private')
+
+    return sc
+
+
+@pytest.fixture
+def user_credentials_not_available():
+    try:
+        _ = get_credentials()
+        should_fail = False
+    except IOError:
+        should_fail = True
+
+    return should_fail
+
+
+@pytest.fixture
+def service_credentials_not_available(service_credentials):
+
+    key_file = service_credentials['key_file']
+    client_email_file = service_credentials['client_file']
+
+    try:
+        _ = create_service_credentials(client_secret_file=client_email_file,
+                                       private_key=key_file)
+        should_fail = False
+    except IOError:
+        should_fail = True
+
+    return should_fail
