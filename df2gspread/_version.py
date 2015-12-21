3acdc1afea4af408cc5f0419e642912035a75c33
etrott <etrott@redhat.com>
2015-12-21 10:31:14 +0100

    Updates version for PyPi

--- df2gspread/_version.py
+++ df2gspread/_version.py
@@ -4,10 +4,10 @@
 # @Date:   2015-09-15 08:57:35
 # @Email:  etrott@redhat.com
 # @Last modified by:   etrott
-# @Last Modified time: 2015-12-10 14:49:08
+# @Last Modified time: 2015-12-21 10:30:04
 
 
 
 
-version_info = ('0', '0', '5')
+version_info = ('0', '0', '6')
 __version__ = '.'.join(version_info[0:3])  # + '-' + version_info[3]
